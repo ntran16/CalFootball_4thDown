a90de0f5eb3bb1f73dda786fec11f10eb3d94e08
raymondhfeng <raymondhfeng@berkeley.edu>
2018-03-04 06:24:50 -0800

    charlie punt logic

--- ray_tests.py
+++ ray_tests.py
@@ -1,1 +1,17 @@
 print("this is a test")
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+import numpy as np
+import glob
+
+pd.set_option("display.max_rows", 50)
+# pd.set_option('display.max_colwidth', -1)
+%matplotlib inline
+
+def curve_fit(x, y, smoothness=.5):
+    from statsmodels.nonparametric.smoothers_lowess import lowess
+    results = lowess(y, x, is_sorted=True, frac=smoothness)
+    return results[:, 1]
+
+
